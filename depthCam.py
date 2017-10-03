7dea96845d0388dcb18bbc7f81bc60aeb4c7e683
wangscu <wangscu@gmail.com>
2017-10-03 14:57:48 +0800

    drug arm

--- depthCam.py
+++ depthCam.py
@@ -41,6 +41,7 @@ userdata = ty.TY_FRAME_DATA()
 
 err = ty.TYFetchFrame(hh, pointer(frame), -1)
 print(err)
+pointBufLen = 0
 
 for i in range(0, frame.validCount):
     image = frame.image[i]
@@ -56,7 +57,7 @@ for i in range(0, frame.validCount):
             rgbBuf = cast(image.buffer, POINTER(c_uint8 * (height * width * 2))).contents
             rgbBuf = np.ctypeslib.as_array(rgbBuf).reshape((height, width, 2))
             img = cv2.cvtColor(rgbBuf, cv2.COLOR_YUV2BGR_YUYV)
-            # cv2.imwrite("wt.png", img)
+            cv2.imwrite("wt.png", img)
             pass
         elif image.pixelFormat == ty.TY_PIXEL_FORMAT_RGB:
             print(image.pixelFormat)
@@ -81,17 +82,19 @@ for i in range(0, frame.validCount):
     elif image.componentID == ty.TY_COMPONENT_POINT3D_CAM:
         pointBuf = cast(image.buffer, POINTER(c_float * (height * width * 3))).contents
         pointBuf = np.ctypeslib.as_array(pointBuf).reshape((height*width, 3))
+        pointBufLen = height*width
         import pcl
         p = pcl.PointCloud(pointBuf)
         pcl.save(p, 'pointcloud.pcd')
 
-vectors = (ty.TY_VECT_3F * (height*width))()
+
+vectors = (ty.TY_VECT_3F * pointBufLen)()
 for i,p in enumerate(pointBuf):
     vectors[i].x = p[0]
     vectors[i].y = p[1]
     vectors[i].z = p[2]
 
-ty.TYRegisterWorldToColor(hh, vectors, 0, height*width, buffer, 1024*1024*5)
+ty.TYRegisterWorldToColor(hh, vectors, 0, pointBufLen, buffer, 1024*1024*5)
 colorBuffer = cast(buffer, POINTER(c_uint16 * (1024 * 512 * 5))).contents
 colorBuffer = np.ctypeslib.as_array(colorBuffer)[:rgbBuf.shape[0]*rgbBuf.shape[1]].reshape((rgbBuf.shape[0], rgbBuf.shape[1], 1))
 
@@ -99,54 +102,66 @@ import matplotlib.pyplot as plt
 
 color = colorBuffer/100 + rgbBuf[:, :, 0:1]/2
 plt.imshow(colorBuffer.reshape((rgbBuf.shape[0], rgbBuf.shape[1])))  # Needs to be in row,col order
-plt.imshow(rgbBuf[:, :, 0:1].reshape((rgbBuf.shape[0], rgbBuf.shape[1])))  # Needs to be in row,col order
+#plt.imshow(rgbBuf[:, :, 0:1].reshape((rgbBuf.shape[0], rgbBuf.shape[1])))  # Needs to be in row,col order
 #plt.imshow(color.reshape(rgbBuf.shape[0], rgbBuf.shape[1]))  # Needs to be in row,col order
-plt.show()
+#plt.show()
 plt.savefig('depth.png')
 
 
-'''
-    import cv2.aruco as aruco
-    import yaml
 
-    cornerPoints = np.array([[[-87.5, 87.5, 0.0], [-57.5, 87.5, 0.0], [-57.5, 57.5, 0.0], [-87.5, 57.5, 0.0]],
+import cv2.aruco as aruco
+import yaml
+
+cornerPoints = np.array([[[-87.5, 87.5, 0.0], [-57.5, 87.5, 0.0], [-57.5, 57.5, 0.0], [-87.5, 57.5, 0.0]],
                              [[57.5, 87.5, 0.0], [87.5, 87.5, 0.0], [87.5, 57.5, 0.0], [57.5, 57.5, 0.0]],
                              [[-87.5, -57.5, 0.0], [-57.5, -57.5, 0.0], [-57.5, -87.5, 0.0], [-87.5, -87.5, 0.0]],
                              [[57.5, -57.5, 0.0], [87.5, -57.5, 0.0], [87.5, -87.5, 0.0], [57.5, -87.5, 0.0]]])
 
-    dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
 
-    dImg = img
+dImg = img
     # dImg = cv2.GaussianBlur(dImg, (5, 5), 0)
     # _, dImg = cv2.threshold(dImg, 200, 255, cv2.THRESH_BINARY)
 
-    corners, ids, rejectedImgPoints = aruco.detectMarkers(dImg, dict)
-    dImgd = aruco.drawDetectedMarkers(img, corners, ids)
+corners, ids, rejectedImgPoints = aruco.detectMarkers(dImg, dict)
+dImgd = aruco.drawDetectedMarkers(img, corners, ids)
 
-    with open('calibration.yaml') as f:
-        loadeddict = yaml.load(f)
-    camera_matrix = np.array(loadeddict.get('camera_matrix'))
-    dist_coeff = np.array(loadeddict.get('dist_coeff'))
 
-    rvecs, tvecs, _objPoints = aruco.estimatePoseSingleMarkers(corners, 30, camera_matrix, dist_coeff)
-    for i in range(0, len(corners)):
-        aruco.drawAxis(dImgd, camera_matrix, dist_coeff, rvecs[i], tvecs[i], 40)
+arm_corners = []
+arm_ids = []
 
-        data = {'rvecs': np.asarray(rvecs[i]).tolist(), 'tvecs': np.asarray(tvecs[i]).tolist()}
-        with open("rt{}.yaml".format(i + 1), "w") as f:
-            yaml.dump(data, f)
+for corner, id in zip(corners, ids):
+    if id[0] > 20:
+        drug_corner = corner[0]
+    else:
+        arm_corners.append(corner[0])
+        arm_ids.append(id[0])
 
-    imgPoints = np.zeros(shape=(0, 2))
-    objectPoints = np.zeros(shape=(0, 3))
-    for i in range(0, len(corners)):
-        id = ids[i]
-        imgPoints = np.append(imgPoints, corners[i][0], axis=0)
-        objectPoints = np.append(objectPoints, cornerPoints[id - 1][0], axis=0)
-    _retval, _rvec, _tvec = cv2.solvePnP(objectPoints, imgPoints, camera_matrix, dist_coeff)
-    aruco.drawAxis(dImgd, camera_matrix, dist_coeff, _rvec, _tvec, 10)
-    data = {'rvec': np.asarray(_rvec).tolist(), 'tvec': np.asarray(_tvec).tolist()}
-    with open("rt_center.yaml", "w") as f:
-        yaml.dump(data, f)
+with open('calibration.yaml') as f:
+    loadeddict = yaml.load(f)
+camera_matrix = np.array(loadeddict.get('camera_matrix'))
+dist_coeff = np.array(loadeddict.get('dist_coeff'))
 
-    cv2.imwrite('aruco_d.jpg', dImgd)
-'''
+rvecs, tvecs, _objPoints = aruco.estimatePoseSingleMarkers(np.array(arm_corners), 30, camera_matrix, dist_coeff)
+for i in range(0, len(arm_corners)):
+    aruco.drawAxis(dImgd, camera_matrix, dist_coeff, rvecs[i], tvecs[i], 40)
+
+if len(drug_corner)>0:
+    drug_rvecs, drug_tvecs, drug_objPoints = aruco.estimatePoseSingleMarkers(np.array([drug_corner]), 40, camera_matrix, dist_coeff)
+    aruco.drawAxis(dImgd, camera_matrix, dist_coeff, drug_rvecs, drug_tvecs, 60)
+
+
+
+imgPoints = np.zeros(shape=(0, 2))
+objectPoints = np.zeros(shape=(0, 3))
+for i in range(0, len(arm_corners)):
+    id = arm_ids[i]
+    imgPoints = np.append(imgPoints, np.array(arm_corners[i]), axis=0)
+    objectPoints = np.append(objectPoints, cornerPoints[id - 1], axis=0)
+_retval, _rvec, _tvec = cv2.solvePnP(objectPoints, imgPoints, camera_matrix, dist_coeff)
+aruco.drawAxis(dImgd, camera_matrix, dist_coeff, _rvec, _tvec, 10)
+data = {'rvec': np.asarray(_rvec).tolist(), 'tvec': np.asarray(_tvec).tolist()}
+with open("rt_center.yaml", "w") as f:
+    yaml.dump(data, f)
+
+cv2.imwrite('aruco_d.jpg', dImgd)
